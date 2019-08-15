3096a057e14f9f5a32bd9a33a81890a1cd8cab50
NeTRuS-dev <53965024+NeTRuS-dev@users.noreply.github.com>
2019-08-15 20:22:04 +0300

    firsr commit

---
+++ Learning/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+
+        loop: true,
+        // pagination: {
+        //     el: '.swiper-pagination',
+        //     type: 'fraction',
+        // },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    });
+
+});
